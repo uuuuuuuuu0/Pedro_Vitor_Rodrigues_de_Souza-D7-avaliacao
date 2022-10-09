b4ad12e8553a778627be580f045a8291048abe16
uuuuuuuuu0 <pedrovrs@usp.br>
2022-10-09 14:00:53 -0300

    App working with db

--- Avaliacao-dotnet/App.xaml.cs
+++ Avaliacao-dotnet/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Avaliacao_dotnet.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Avaliacao_dotnet
 {
@@ -13,5 +16,26 @@ namespace Avaliacao_dotnet
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+
+            services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlite("Data source = Users.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
     }
 }
